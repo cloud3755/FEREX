c6f2eb65ecb5081765e79dce77073145ba40ca26
Unknown <arturodlt83@gmail.com>
2018-07-23 22:22:09 -0500

    se cambia el nombre d eingles a español

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Salir
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -104,6 +104,6 @@
         <script src="{{ asset('js//bootstrap/bootstrap-select.js') }}"></script>
     <!--  -->
     @show
-    
+
 </body>
 </html>
