fdee5f55d678364871c723a530acf287c973aa12
Unknown <arturodlt83@gmail.com>
2018-08-03 00:02:02 -0500

    se agrega vista del cliente y se agrega env XD

--- resources/views/Clientes/AltaClientes.blade.php
+++ resources/views/Clientes/AltaClientes.blade.php
@@ -4,7 +4,174 @@
 @endsection
 @section('content')
 
+<div class="container">
 
+  <div class="panel panel-default">
+      <div class="panel-heading">
+        <span>Nuevo cliente</span>
+        <button id="btnAgregar" class="btn btn-success">Agregar</button>
+        <button id="btnCerrar" class="btn btn-danger">Cerrar</button>
+      </div>
+
+      <div hidden id="panelAgregar" class="panel-body">
+          @section('mensajesBackEnd')
+            @parent
+          @endsection
+          <form class="form" method="POST" action="/productos/nuevo">
+           {{ csrf_field() }}
+            <div class="row">
+              <div class="col-md-6">
+
+                <div class="input-group">
+                  <label class="sr-only" for="nombre">Nombre</label>
+                  <div class="input-group-addon">Nombre</div>
+                  <input type="text" class="form-control" name="nombre" id="nombre" placeholder="nombre" required>
+                </div>
+
+              </div>
+              <div class="col-md-6">
+
+                <div class="input-group">
+                  <label class="sr-only" for="descripcion">Razon social</label>
+                  <div class="input-group-addon">Razon social</div>
+                  <textarea rows="1" class="form-control" name="RazonSocial" id="descripcion" placeholder="Razon Social"></textarea>
+                </div>
+
+              </div>
+            </div>
+            <br/>
+            <div class="row">
+              <div class="col-md-4">
+
+                <div class="input-group">
+
+                  <label class="sr-only" for="claveProdServ">Contacto</label>
+                  <div class="input-group-addon">Contacto</div>
+                  <input type="text" class="form-control" name="Contacto" id="claveProdServ" placeholder="Contacto">
+
+                </div>
+
+              </div>
+              <div class="col-md-4">
+
+                <div class="input-group">
+                  <label class="sr-only" for="minimoAlarma">RFC</label>
+                  <div class="input-group-addon">RFC </div>
+                  <input type="text" class="form-control" name="Rfc" id="minimoAlarma" placeholder="RFC">
+                </div>
+
+              </div>
+              <div class="col-md-4">
+
+                  <div class="input-group">
+
+                    <label class="sr-only" for="codigoBarras">Correo</label>
+                    <div class="input-group-addon">Correo</div>
+                    <input type="text"  class="form-control" name="Correo" id="codigoBarras" placeholder="Correo">
+
+                  </div>
+
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+              <div class="col-md-4">
+
+                <div class="input-group">
+
+                  <label class="sr-only" for="unidadMedida">Limte de Credito</label>
+                  <div class="input-group-addon">Limite de Credito</div>
+                  <input type="number" min="0.01"  class="form-control" name="LimiteDeCredito" id="precioA" placeholder="LimiteDeCredito">
+
+                </div>
+
+              </div>
+              <div class="col-md-4">
+
+                  <div class="input-group">
+
+                    <label class="sr-only" for="unidadMedida">Telefono 1</label>
+                    <div class="input-group-addon">Telefono 1</div>
+                    <input type="text"   class="form-control" name="Telefono1" id="precioB" placeholder="Telefono1">
+
+                  </div>
+
+                </div>
+                <div class="col-md-4">
+
+                    <div class="input-group">
+
+                      <label class="sr-only" for="unidadMedida">Direccion</label>
+                      <div class="input-group-addon">Direccion</div>
+                      <input type="text"  class="form-control" name="precioC" id="precioC" placeholder="Direccion">
+
+                    </div>
+
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="text-center">
+              <button type="submit"  class="btn btn-primary">Agregar</button>
+              <button class="btn btn-warning" onclick="$('form').reset">Limpiar datos</button>
+            </div>
+          <!-- Modal -->
+          <div class="modal fade" id="modalExistencia" tabindex="-1" role="dialog" aria-labelledby="modalExistenciaLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="modalExistenciaLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Modal -->
+          </form>
+
+      </div>
+  </div>
+
+  <div class="panel panel-default">
+      <div class="panel-heading">Productos</div>
+
+      <div class="panel-body">
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Numero de Cliente</th>
+                  <th>Nombre</th>
+                  <th>Razon Social</th>
+                  <th>Contacto</th>
+                  <th>Rfc</th>
+                  <th>Correo</th>
+                  <th>Limite de credito</th>
+                  <th>Telefonos</th>
+                  <th>Consumo</th>
+                  <th>Status</th>
+                  <th>Direccion</th>
+
+                </tr>
+              </thead>
+              <tbody>
+
+              </tbody>
+            </table>
+          </div>
+      </div>
+  </div>
+
+</div>
 
 
 @endsection
