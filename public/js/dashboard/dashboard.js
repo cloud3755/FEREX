fda7420c65a65b80856e83dfa72bee354c06da55
Unknown <arturodlt83@gmail.com>
2018-08-15 23:12:06 -0500

    se modifica codigo de javascript al archivo de dashboard

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -67,30 +67,43 @@ function drawAxisTickColors() {
       chart.draw(data, options);
     }
 
-// function drawChart() {
-//   var mes = 'Sin mes';
-//   var data = google.visualization.arrayToDataTable([
-//     ['Mes', 'Ventas'],
-//     ['2013',  1000],
-//     ['2014',  1170],
-//     ['2015',  660],
-//     ['2016',  1030],
-//     ['2017',  1030],
-//     ['2018',  1030],
-//     ['2019',  1030],
-//     ['2020',  1030],
-//     ['2021',  1030]
-//   ]);
-//
-//   var options = {
-//     title: 'Company Performance',
-//     hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-//     vAxis: {minValue: 0}
-//   };
-//
-//   var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-//   chart.draw(data, options);
-// }
+    function drawChart() {
+      var mes = 'Sin mes';
+
+      var datosMes = new Array();
+      datosMes[0] = ['Mes', 'Ventas'];
+
+      for (var i = 0; i < ventasMes.length; i++)
+       {
+
+         if (ventasMes[i].mes == 1) {mes = 'Enero';}
+         else if (ventasMes[i].mes == 2) {mes = 'Febrero';}
+         else if (ventasMes[i].mes == 3) {mes = 'Marzo';}
+         else if (ventasMes[i].mes == 4) {mes = 'Abril';}
+         else if (ventasMes[i].mes == 5) {mes = 'Mayo';}
+         else if (ventasMes[i].mes == 6) {mes = 'Junio';}
+         else if (ventasMes[i].mes == 7) {mes = 'Julio';}
+         else if (ventasMes[i].mes == 8) {mes = 'Agosto';}
+         else if (ventasMes[i].mes == 9) {mes = 'Septiembre';}
+         else if (ventasMes[i].mes == 10) {mes = 'Octubre';}
+         else if (ventasMes[i].mes == 11) {mes = 'Noviembre';}
+         else if (ventasMes[i].mes == 12) {mes = 'Diciembre';}
+
+         datosMes[i + 1] = [ mes, ventasMes[i].suma ];
+       }
+
+
+      var data = google.visualization.arrayToDataTable(datosMes,false);
+
+      var options = {
+        title: 'Ventas por mes',
+        hAxis: {title: 'Ventas',  titleTextStyle: {color: '#333'}},
+        vAxis: {minValue: 0}
+      };
+
+      var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
 
 function drawChart1() {
   var data = google.visualization.arrayToDataTable([
